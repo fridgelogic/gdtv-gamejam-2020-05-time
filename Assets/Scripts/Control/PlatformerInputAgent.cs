15fb2174e118fdeb1a1078d983bbc1cdee94a9b3
Trevor McDonald <trevor.j.mcdonald.personal@gmail.com>
2020-05-03 22:01:04 -0400

    Fixed PlayerInput by recreating input mapping

--- Assets/Scripts/Control/PlatformerInputAgent.cs
+++ Assets/Scripts/Control/PlatformerInputAgent.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FridgeLogic.Movement;
+﻿using FridgeLogic.Movement;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,7 +7,6 @@ namespace FridgeLogic.Control
     [RequireComponent(typeof(IMovement2D))]
     public class PlatformerInputAgent : MonoBehaviour
     {
-        private PlatformerInputMap inputMap = null;
         private IMovement2D movement = null;
 
         private void ProcessMovement(Vector2 inputVector)
@@ -23,44 +21,24 @@ namespace FridgeLogic.Control
         }
 
         #region Input Events
-        private void OnMove(InputAction.CallbackContext context)
+        public void OnMove(InputAction.CallbackContext context)
         {
             ProcessMovement(context.ReadValue<Vector2>());
         }
 
-        private void OnJump(InputAction.CallbackContext context)
+        public void OnJump(InputAction.CallbackContext context)
         {
-            ProcessJump();
+            if (context.performed)
+            {
+                ProcessJump();
+            }
         }
         #endregion
 
         #region Unity Lifecycle
-        private void Awake()
-        {
-            
-            inputMap = new PlatformerInputMap();
-            inputMap.Gameplay.Move.performed += OnMove;
-            inputMap.Gameplay.Jump.performed += OnJump;
-        }
-
         private void Start()
         {
             movement = GetComponent<IMovement2D>();
-        }
-
-        private void OnEnable()
-        {
-            inputMap.Enable();
-        }
-
-        private void OnDisable()
-        {
-            inputMap.Disable();
-        }
-
-        private void Update()
-        {
-            
         }
         #endregion
     }
